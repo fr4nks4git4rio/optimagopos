1937a011b3e2cd538bc2c2a108c1616481e0bf92
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-04-09 10:48:58 -0500

    reporte de logs

--- resources/views/livewire/reportes/logs.blade.php
+++ resources/views/livewire/reportes/logs.blade.php
@@ -47,10 +47,10 @@
             <tbody>
                 @forelse($logs as $log)
                     <tr>
-                        <td>{{ $log['fecha'] }}</td>
-                        <td>{{ $log['log'] }}</td>
-                        <td>{{ $log['data'] }}</td>
-                        <td>{{ $log['status'] }}</td>
+                        <td>{{ $log->fecha }}</td>
+                        <td>{{ $log->log }}</td>
+                        <td>{{ $log->data }}</td>
+                        <td>{{ $log->status }}</td>
                     </tr>
                 @empty
                     <tr>
