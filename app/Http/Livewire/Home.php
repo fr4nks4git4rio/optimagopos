66ccba1c882e7d4fec0362169be872b77757db4c
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-06-02 09:52:34 -0500

    entrega_20260529

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -139,12 +139,13 @@ class Home extends Component
                     ->get()
                     ->count();
                 $this->resumenData['ventas_netas_operacion'] = DB::table('tb_tickets as ticket')
-                    ->selectRaw("ticket.importe, ticket.id as id, HOUR(ticket.fecha_transaccion) as hora")
+                    ->selectRaw("ticket.importe, ticket.id as id")
                     ->leftJoin('tb_ticket_operaciones as to', 'ticket.id', 'to.ticket_id')
                     ->leftJoin('tb_sucursal_forma_pagos as sfp', 'sfp.id', 'to.sucursal_forma_pago_id')
                     ->leftJoin('tb_sucursales as sucursal', 'sucursal.id', 'ticket.sucursal_id')
                     ->where('sucursal.cliente_id', user()->cliente_id)
-                    ->whereRaw("HOUR(ticket.fecha_transaccion) = ?", [now()->hour])
+                    ->orderByRaw("HOUR(ticket.fecha_transaccion) asc")
+                    ->take(20)
                     ->get()
                     ->mapWithKeys(function ($item) {
                         return [$item->id => $item->importe];
