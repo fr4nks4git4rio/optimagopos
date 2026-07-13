27d58e1a6991e5ac6a3b3d154396f4b663bc6ce3
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-07-13 13:25:28 -0500

    bug

--- app/Http/Livewire/Suscripciones/Activar.php
+++ app/Http/Livewire/Suscripciones/Activar.php
@@ -170,7 +170,7 @@ class Activar extends Modal
             $this->emit('closeModal');
         } catch (\Exception $e) {
             DB::rollBack();
-            Log::error("Error al intentar activar la suscripcion. Error: {$e->getMessage()}");
+            Log::error("Error al intentar activar la suscripcion. Error: {$e->getMessage()}. {$e->getTrace()}");
             $this->emit('show-toast', 'Error al activar la suscripción.', 'danger');
             return;
         }
