045507de0db5e1e9a135da05ba4d0bd62fe7f295
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-08-28 12:31:07 -0500

    arreglos

--- resources/views/livewire/secciones-dashboard/impuestos.blade.php
+++ resources/views/livewire/secciones-dashboard/impuestos.blade.php
@@ -1,8 +1,9 @@
-@can('dashboardResume-viewTaxes' && $this->con_impuestos)
+@if (user()->can('dashboardResume-viewTaxes') && $this->con_impuestos)
     <div class="row g-3 mb-3 px-1">
         @foreach ($impuestosData['impuestos'] as $impuesto)
             <div class="col-12 col-sm-6 col-md-3">
-                <div class="card border-0 border-start border-danger bg-dark-subtle shadow-sm border-4 text-center h-100">
+                <div
+                    class="card border-0 border-start border-danger bg-dark-subtle shadow-sm border-4 text-center h-100">
                     <div class="card-body align-items-center d-flex flex-column">
                         <span class="fs-5 fw-bold text-uppercase">{{ $impuesto->nombre }}</span>
                         <span class="fs-3 text-danger m-auto">
@@ -14,4 +15,4 @@
             </div>
         @endforeach
     </div>
-@endcan
+@endif
