70687c9dc9d23fe0c16ac042817e63266bbabbd0
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-09-04 11:20:14 -0500

    translate

--- resources/views/livewire/reportes/articulos-vendidos.blade.php
+++ resources/views/livewire/reportes/articulos-vendidos.blade.php
@@ -1,9 +1,9 @@
-@section('title', 'Artículos Vendidos')
+@section('title', __('site.reports.articles_sold.title'))
 
 <div>
     <div wire:loading.delay.longer>
         <div class="loading">
-            <div class="spinner-border text-primary my-3" role="status"><span class="visually-hidden">Cargando...</span></div>
+            <div class="spinner-border text-primary my-3" role="status"><span class="visually-hidden">{{ __('site.common.loading') }}</span></div>
         </div>
     </div>
 
